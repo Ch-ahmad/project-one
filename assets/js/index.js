5a8370e45d6e245a6be2e904f62f12759ccccd93
Ahmad Yamien <ayamien@hawk.iit.edu>
2022-03-23 14:28:50 -0500

    Added support for label instead of placeholder

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,21 @@ if(path === '/page2/') {
         alert("Success!");
     }
 }
+
+let inputs = document.getElementsByTagName("input");
+console.log(inputs);
+for(let i=0; i<inputs.length; i++) {
+    inputs[i].onchange = function() {
+        console.log("FIRED!")
+        let inp = inputs[i];
+        if(inp.value === "") {
+            inp.classList.remove("input-filled");
+        } else {
+            if(!inp.classList.contains("input-filled")) {
+                inp.classList.add("input-filled");
+            }
+        }
+    }
+}
+
+
